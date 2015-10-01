17bff7fcc812e7591414ced17fd29bd4601c5439
OlegKlimenkoGitHub <oleg.v.klimenko@gmail.com>
2015-10-01 09:17:50 +0300

    Update 01.10.2015

--- Battle.Domain/Concrete/Context.cs
+++ Battle.Domain/Concrete/Context.cs
@@ -12,7 +12,6 @@ namespace Battle.Domain.Concrete
         public UsersContext()
             : base("DefaultConnection")
             //: base("Entities")
-            //: base("EFDbContext")
         {
         }
 
@@ -22,63 +21,63 @@ namespace Battle.Domain.Concrete
 
     public class CountryInitContext : DbContext
     {
-        // В данном случае "EFDbContext" - имя подключения к базе данных
+        // В данном случае "DefaultConnection" - имя подключения к базе данных
         // которое настроено в файле web.config в корне MVC проекта.
-        public CountryInitContext() : base("EFDbContext") { }
+        public CountryInitContext() : base("DefaultConnection") { }
         public DbSet<CountryInit> InitCountries { get; set; }
     }
 
     public class CountryContext : DbContext
     {
-        // В данном случае "EFDbContext" - имя подключения к базе данных
+        // В данном случае "DefaultConnection" - имя подключения к базе данных
         // которое настроено в файле web.config в корне MVC проекта.
-        public CountryContext() : base("EFDbContext") { }
+        public CountryContext() : base("DefaultConnection") { }
         public DbSet<Country> Countries { get; set; }
     }
 
     public class GamerContext : DbContext
     {
-        // В данном случае "EFDbContext" - имя подключения к базе данных
+        // В данном случае "DefaultConnection" - имя подключения к базе данных
         // которое настроено в файле web.config в корне MVC проекта.
-        public GamerContext() : base("EFDbContext") { }
+        public GamerContext() : base("DefaultConnection") { }
         public DbSet<Gamer> Gamers { get; set; }
     }
 
     public class DesignContext : DbContext
     {
-        // В данном случае "EFDbContext" - имя подключения к базе данных
+        // В данном случае "DefaultConnection" - имя подключения к базе данных
         // которое настроено в файле web.config в корне MVC проекта.
-        public DesignContext() : base("EFDbContext") { }
+        public DesignContext() : base("DefaultConnection") { }
         public DbSet<Design> Designes { get; set; }
     }
 
     public class ArmyContext : DbContext
     {
-        // В данном случае "EFDbContext" - имя подключения к базе данных
+        // В данном случае "DefaultConnection" - имя подключения к базе данных
         // которое настроено в файле web.config в корне MVC проекта.
-        public ArmyContext() : base("EFDbContext") { }
+        public ArmyContext() : base("DefaultConnection") { }
         public DbSet<Army> Armies { get; set; }
     }
 
     public class BattleMechContext : DbContext
     {
-        // В данном случае "EFDbContext" - имя подключения к базе данных
+        // В данном случае "DefaultConnection" - имя подключения к базе данных
         // которое настроено в файле web.config в корне MVC проекта.
-        public BattleMechContext() : base("EFDbContext") { }
+        public BattleMechContext() : base("DefaultConnection") { }
         public DbSet<Battlemech> BattleMechs { get; set; }
     }
 
     public class EvContext : DbContext
     { 
-        public EvContext() : base("EFDbContext") {}
+        public EvContext() : base("DefaultConnection") {}
         public DbSet<Ev> Events {get; set;}
     }
 
     public class ManufactureContext : DbContext
     {
-        // В данном случае "EFDbContext" - имя подключения к базе данных
+        // В данном случае "DefaultConnection" - имя подключения к базе данных
         // которое настроено в файле web.config в корне MVC проекта.
-        public ManufactureContext() : base("EFDbContext") { }
+        public ManufactureContext() : base("DefaultConnection") { }
         public DbSet<Manufacture> Manufactures { get; set; }
     }
 
